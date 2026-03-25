f51bcb723e6d9c6c6a296730bfde607c6773e12e
Mátyás Fekete <matyasfekete@Matyass-MacBook.local>
2026-03-25 21:42:49 +0100

    Direct connection to hr-app2 database

--- js/store.js
+++ js/store.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app, "hr-app2");
 
 /**
  * Store class manages Firestore interactions with optimistic local state.
